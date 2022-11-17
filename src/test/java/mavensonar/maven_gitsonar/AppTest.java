a8ff042a8f01d55ee2ccfb789bcd5dffb1d00f25
prasa <prasa@LAPTOP-H0PSVFQQ>
2022-11-17 19:08:51 +0530

    commit for maven,git,jenkins and sonar

---
+++ src/test/java/mavensonar/maven_gitsonar/AppTest.java
@@ -0,0 +1,20 @@
+package mavensonar.maven_gitsonar;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
